15ce07e4418bbcd93a16d45a7e6bde2a4a70237f
Fadli305 <109408701+Fadli305@users.noreply.github.com>
2024-11-10 16:29:11 +0700

    Update App.jsx
    tambah berita

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,21 @@ import BlogArticle3 from './pages/BlogArticle3';
 import BlogArticle4 from './pages/BlogArticle4';
 import BlogArticle5 from './pages/BlogArticle5';
 import BlogArticle6 from './pages/BlogArticle6';
+import Berita1 from './pages/Berita/Berita1';
+import Berita2 from './pages/Berita/Berita2';
+import Berita3 from './pages/Berita/Berita3';
+import Berita4 from './pages/Berita/Berita4';
+import Berita5 from './pages/Berita/Berita5';
+import Berita6 from './pages/Berita/Berita6';
+import Berita7 from './pages/Berita/Berita7';
+import Berita8 from './pages/Berita/Berita8';
+import Berita9 from './pages/Berita/Berita9';
+import Berita10 from './pages/Berita/Berita10';
+import Berita11 from './pages/Berita/Berita11';
+import Berita12 from './pages/Berita/Berita12';
+import Berita13 from './pages/Berita/Berita13';
+import Berita14 from './pages/Berita/Berita14';
+import DashboardBerita from './pages/Berita/DashboardBerita';
 
 export default function App() {
   return (
@@ -44,6 +59,24 @@ export default function App() {
           <Route path="/blogartikel4" element={<BlogArticle4 />} />
           <Route path="/blogartikel5" element={<BlogArticle5 />} />
           <Route path="/blogartikel6" element={<BlogArticle6 />} />
+
+          {/* Dashboard Berita */}
+          <Route path="/berita" element={<DashboardBerita/>} />
+          {/* Laman Berita */}
+          <Route path="/berita/1" element={<Berita1 />} />
+          <Route path="/berita/2" element={<Berita2 />} />
+          <Route path="/berita/3" element={<Berita3 />} />
+          <Route path="/berita/4" element={<Berita4 />} />
+          <Route path="/berita/5" element={<Berita5 />} />
+          <Route path="/berita/6" element={<Berita6 />} />
+          <Route path="/berita/7" element={<Berita7 />} />
+          <Route path="/berita/8" element={<Berita8 />} />
+          <Route path="/berita/9" element={<Berita9 />} />
+          <Route path="/berita/10" element={<Berita10 />} />
+          <Route path="/berita/11" element={<Berita11 />} />
+          <Route path="/berita/12" element={<Berita12 />} />
+          <Route path="/berita/13" element={<Berita13 />} />
+          <Route path="/berita/14" element={<Berita14/>} />
           
           {/* Tentang Kami */}
           <Route path="/tentang-kami" element={<TentangKami />} />
